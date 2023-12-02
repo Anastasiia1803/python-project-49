6a1e111f19769d9df5dfb7b44557fc2e4ee57cde
Anastasiia1803 <milyaniuk2014@gmail.com>
2023-12-02 11:45:56 +0300

    brain-games

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -15,5 +15,5 @@ def calc():
         correct_answer = random_number_1 - random_number_2
     else:
         correct_answer = random_number_1 * random_number_2
-
-    return f'{random_number_1} {operation} {random_number_2}', str(correct_answer)
+    question = f'{random_number_1} {operation} {random_number_2}'
+    return question, str(correct_answer)
